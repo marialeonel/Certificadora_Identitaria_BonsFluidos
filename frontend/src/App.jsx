b0a43a4c429bb8607c6b852deb049f7ca5bb9b48
Gabriella Ribeiro <gabx2205@gmail.com>
2024-11-21 20:41:50 -0300

    fix: imports adjustments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/landing-page/LandingPage';
-import Blog from './components/blog/Blog';
-import Login from './components/login/Login';
+import Home from './pages/home/Home';
+import Blog from './pages/blog/Blog';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/login" element={<Login />} />
       </Routes>
