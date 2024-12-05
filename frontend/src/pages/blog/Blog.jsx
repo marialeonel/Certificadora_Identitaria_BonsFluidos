5550f254a8b5bae965a296bc7b8487194cd4da31
GiDFerreira <giovannaferreira@alunos.utfpr.edu.br>
2024-12-05 18:29:47 -0300

    Adicionando componentes na página blog

--- frontend/src/pages/blog/Blog.jsx
+++ frontend/src/pages/blog/Blog.jsx
@@ -1,14 +1,35 @@
 import { useNavigate } from 'react-router-dom';
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
+
 
 function Blog() {
   const navigate = useNavigate();
 
   return (
+    <>
+    <Header/>
     <div>
       <h1>Blog</h1>
       <p>Esta é a página do blog onde você pode visualizar posts.</p>
       <button onClick={() => navigate('/login')}>Login</button>
+        <div className="bg-logo-color p-20">
+          <h1 className="text-4xl font-bold text-center mb-3 text-white">
+            BLOG
+          </h1>
+          <p className="text-xl text-center text-white">
+            Novidades do projeto Bons Fluídos
+          </p>
+        </div>
+        <div>
+          <Input className="w-full px-4 py-2" placeholder="Pesquise aqui..."/>
+          <Button>+ Adicionar post</Button>
+        </div>
     </div>
+    <Footer/>
+    </>
   );
 }
 
