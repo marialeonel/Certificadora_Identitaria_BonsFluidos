bfeb4bf44aa946b9d87691d3dc9070b9ef4550e8
Clara <claraafurinii@gmail.com>
2024-12-05 07:28:32 -0300

    Agenda Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import Home from './pages/home/Home';
-import Blog from './pages/blog/Blog';
-import Login from './pages/login/Login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Home from "./pages/home/Home";
+import Blog from "./pages/blog/Blog";
+import Login from "./pages/login/Login";
+import Agenda from "./pages/agenda/Agenda";
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/agenda" element={<Agenda />} />
         </Routes>
-    </Router>
+      </Router>
     </AuthProvider>
   );
 }
