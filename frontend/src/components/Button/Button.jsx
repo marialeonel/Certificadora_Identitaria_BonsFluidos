0305184921d927e97e8ec4cabbb2a74321983805
GiDFerreira <giovannaferreira@alunos.utfpr.edu.br>
2024-12-08 19:24:41 -0300

    Tirando o width no componente
    Eu tirei o width porque na hora de editar o botão tava paia

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,10 +1,12 @@
+import React from "react"
+
 function Button({ className, onClick, type='button', icon, children }) {
     return (
         <>
             <button
                 type={type}
                 onClick={onClick}
-                className={`flex w-[100%] justify-center items-center rounded-3xl bg-logo-color px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-dark hover:outline hover:outline-1  ${icon ? 'gap-2' : ''} ${className}`}
+                className={`flex justify-center items-center rounded-3xl bg-logo-color px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-dark hover:outline hover:outline-1  ${icon ? 'gap-2' : ''} ${className}`}
             >
             {icon && <img src={icon} className={`w-4 h-4`}/>}
             {children}
