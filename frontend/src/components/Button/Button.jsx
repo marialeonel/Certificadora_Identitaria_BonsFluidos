7d6ffbc8ae0d5a5468f22a5425fcde6b4330135f
Gabriella Ribeiro <gabx2205@gmail.com>
2024-12-11 20:40:25 -0300

    fix: button adjustments

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -6,7 +6,7 @@ function Button({ className, onClick, type='button', icon, children }) {
             <button
                 type={type}
                 onClick={onClick}
-                className={`flex justify-center items-center rounded-3xl bg-logo-color px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-dark hover:outline hover:outline-1  ${icon ? 'gap-2' : ''} ${className}`}
+                className={`flex w-[100%] justify-center items-center rounded-3xl bg-logo-color px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-dark hover:outline hover:outline-1  ${icon ? 'gap-2' : ''} ${className}`}
             >
             {icon && <img src={icon} className={`w-4 h-4`}/>}
             {children}
