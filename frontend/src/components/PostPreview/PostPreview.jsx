a488dab94ddd7e10099f7c375d5341313163187e
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2024-12-11 23:24:45 -0300

    fix: image cors error

--- frontend/src/components/PostPreview/PostPreview.jsx
+++ frontend/src/components/PostPreview/PostPreview.jsx
@@ -1,7 +1,10 @@
 function PostPreview({ post }) {
+    const baseUrl = "http://localhost:3000/uploads/";
+    const imageUrl = `${baseUrl}${post.imageUrl}`;
+
     return (
         <div className='group w-[320px] hover:shadow-[0_4px_8px_rgba(0,0,0,0.2)] rounded-lg transition-shadow duration-300'>
-            <img src={post.imageUrl} className='w-full h-[300px] transition-all duration-300 rounded-lg group-hover:rounded-b-none'/>
+            <img crossorigin="anonymous" src={imageUrl} className='w-full h-[300px] transition-all duration-300 rounded-lg group-hover:rounded-b-none'/>
             <div className='flex flex-col h-[150px] justify-center items-center px-3 gap-6'>
                 <h1 className='text-xl font-bold'>{post.title}</h1>
                 <div className='flex w-[100%] justify-between text-sm text-gray-500'>
